0a826f2f7e3819f6fbd6babff9ac50e5e9375a53
Stuart Jackson <stuart.2.jackson@me.com>
2019-12-19 09:14:16 +0000

    more anotaions

--- pi-ping.py
+++ pi-ping.py
@@ -7,7 +7,7 @@ from datetime import datetime
 #IP = '192.168.1.254' #ping router gatway IP address
 IP = '8.8.8.8' #ping google DNS server 
 
-GPIO.setwarnings(False)
+GPIO.setwarnings(False) # stops errer message due to pins in old state may need to look at a cleaner way  to do this
 GPIO.setmode(GPIO.BCM) # set board mode to Broadcom
 GPIO.setup(21, GPIO.OUT) # set up pin 17
 
@@ -15,14 +15,14 @@ while True:
   response = os.system("ping -c 1 {}" .format(IP))
   if response == 0:
     print ('internet is up!')
-    GPIO.output(21, 0) # turn off pin 21
-    time.sleep(0.5)
+    GPIO.output(21, 0) # turn off pin 21 LED
+    time.sleep(0.5) # sleep for 1/2 a second
   else:
-    now = datetime.now()  
-    f= open("ping_log.log","a+")
-    f.write(str(now))
-    f.write("  internet is down\n")
-    f.close()
+    now = datetime.now()  # sets now with current data and time to that the logs can be time stamped
+    f= open("ping_log.log","a+")  # open a file called ping_log.log in apend mode 
+    f.write(str(now))    # write the curent time to the ping_log file
+    f.write("  internet is down\n") # print internet is down text to the log file
+    f.close()   # close the file
     print ('internet is down!')
-    GPIO.output(21, 1) # turn on pin 21
-    time.sleep(0.5)
+    GPIO.output(21, 1) # turn on pin 21 LED
+    time.sleep(0.5) # sleep for 1/2 a second
