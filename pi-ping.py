c13994af42c13a2b1affbc60ed6c92fafaf6dbaa
Stuart Jackson <stuart.2.jackson@me.com>
2019-12-20 13:30:18 +0000

    improved date/time format

--- pi-ping.py
+++ pi-ping.py
@@ -14,13 +14,13 @@ GPIO.setup(21, GPIO.OUT) # set up pin 17
 while True:
   response = os.system("ping -c 1 {}" .format(IP))
   if response == 0:
-    print ('internet is up!')
+    print ("internet is up!")
     GPIO.output(21, 0) # turn off pin 21 LED
     time.sleep(0.5) # sleep for 1/2 a second
   else:
     now = datetime.now()  # sets now with current data and time to that the logs can be time stamped
-    f= open("/var/www/html/ping_log.html","a+")  # open a file called ping_log.log in apend mode 
-    f.write(str(now))    # write the curent time to the ping_log file
+    f= open("/var/www/html/ping_log.html","a+")  # open a html file called ping_log.html in apend mode 
+    f.write(now.strftime("%c"))    # write the curent time to the ping_log file
     f.write("  internet is down<br>\n") # print internet is down text to the log file
     f.close()   # close the file
     print ('internet is down!')
